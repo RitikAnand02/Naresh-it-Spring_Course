2ef091f4e9b3758607a6ff30966d7e73ccc34595
RitikAnand02 <ritikanand.02062000@gmail.com>
2025-04-11 21:43:22 +0530

    new

---
+++ Naresh it SPBMS Class_Code/Boot_IOCProj14-MiniProject-Profiles_Single_YML/src/test/java/com/nt/BootIocProj014MiniProject_ProfilesTests.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootIocProj014MiniProject_ProfilesTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
